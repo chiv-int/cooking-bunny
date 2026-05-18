2d31ae6a9a007b4dab817e770a41d47ffbb7a5bb
ChivInthera <chivinthera@gmail.com>
2026-05-18 11:59:34 +0700

    npc interaction & enviorment as me

--- bunny-power/player/player.gd
+++ bunny-power/player/player.gd
@@ -20,3 +20,5 @@ func _physics_process(_delta: float) -> void:
 func update_blend_positions(direction_vector: Vector2) -> void:
 	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", direction_vector)
 	animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position", direction_vector)
+func player() -> void:
+	pass
