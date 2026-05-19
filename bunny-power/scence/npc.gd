0a6d3c6c15f02fbf9bdb826717c7b9c19a3d3d78
Chiv_int <chivinthera@gmail.com>
2026-05-19 13:46:43 +0700

    added a physical dialog & ingredients
    make a ui for the npc dialog, connect it together , and added the ingredient assets

--- bunny-power/scence/npc.gd
+++ bunny-power/scence/npc.gd
@@ -4,10 +4,12 @@ var is_chatting = false
 var player_in_chat_zone = false
 
 var dialogue = [
-	"Hello cousin! Do you miss home?",
-	"How was the city like?",
-	"I heard you are going to cook for all of us...",
-	"Is that true?",
+	"wasssuppp, bunny",
+	"How uni going for you?",
+	". . .",
+	"i heard you're the family chef this year?",
+	"GoodLuck,ah bun!",
+	
 ]
 var dialogue_index = 0
 
@@ -16,17 +18,12 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("interact"):
-		print("Z pressed! in zone: ", player_in_chat_zone)
 		if player_in_chat_zone and not is_chatting:
 			_start_chat()
-		elif is_chatting:
-			_show_next_line()
 
 func _on_chat_detection_area_body_entered(body: Node2D) -> void:
-	# Only react to the player, ignore other bodies like slimes etc
 	if body.is_in_group("player"):
 		player_in_chat_zone = true
-		print("ready to chat - press Z")
 
 func _on_chat_detection_area_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
@@ -37,15 +34,10 @@ func _on_chat_detection_area_body_exited(body: Node2D) -> void:
 func _start_chat() -> void:
 	is_chatting = true
 	dialogue_index = 0
-	_show_next_line()
-
-func _show_next_line() -> void:
-	if dialogue_index < dialogue.size():
-		print(dialogue[dialogue_index])
-		dialogue_index += 1
-	else:
-		_end_chat()
+	# queue all lines at once into the textbox
+	for line in dialogue:
+		Textbox.queue_text(line)
 
 func _end_chat() -> void:
 	is_chatting = false
-	print("=== chat ended ===")
+	Textbox.hide_textbox()
